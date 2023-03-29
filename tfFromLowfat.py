e1f0f8780fd1da2e40f1bbc4467d1bc9541ce79d
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-03-29 14:23:00 +0200

    initial loading of data

---
+++ tfFromLowfat.py
@@ -0,0 +1,38 @@
+import os
+from tf.convert.xml import XML
+
+
+TEST_SET = set(
+    """
+    26-jude.xml
+    """.strip().split()
+)
+
+AUTHOR = "Evangelists and apostles"
+TITLE = "Greek New Testament"
+INSTITUTE = "ETCBC (Eep Talstra Centre for Bible and Computer)"
+
+GENERIC = dict(
+    author=AUTHOR,
+    title=TITLE,
+    institute=INSTITUTE,
+    language="nl",
+    converters="Dirk Roorda et al. (Text-Fabric)",
+    sourceFormat="XML (lowfat)",
+    descriptionTf="Nestle 1904 edition",
+)
+
+
+def transform(text):
+    return text
+
+
+X = XML(
+    sourceVersion="nestle1904",
+    testSet=TEST_SET,
+    generic=GENERIC,
+    transform=transform,
+    tfVersion="0.1",
+)
+
+X.run(os.path.basename(__file__))
