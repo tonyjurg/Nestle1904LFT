aec505a2e60d5ead3d6508679c957407e4d5b0ea
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-04-05 16:45:09 +0200

    commit

--- tf/0.1.1/Rule.tf
+++ tf/0.1.1/Rule.tf
@@ -1,7 +1,7 @@
 @node
 @author=Evangelists and apostles
 @converters=Tony
-@description=this is XML attribute Rule
+@description=this is XML attribute rule
 @descriptionTf=Nestle 1904 edition
 @institute=ETCBC (Eep Talstra Centre for Bible and Computer)
 @language=el
@@ -12,21 +12,300 @@
 @writtenBy=Text-Fabric
 @dateWritten=2023-04-03T16:01:11Z
 
-1849	ClCl
-1852	ClCl
-1863	ClCl2
-1876	ClCl
-1884	ClCl
-1911	ClCl
-1931	ClCl2
-1943	ClCl2
-1976	ClCl
-2006	ClCl2
-2018	ClCl
-ClCl
-2029	ClCl
-2039	ClCl2
-2067	ClCl
-2095	ClCl2
-2110	ClCl2
-2141	ClCl
+1799	CLaCL
+S-IO
+DetAdj
+NP-CL
+NpaNp
+AdjpNp
+1806	DetNP
+NPofNP
+O-V-ADV-S-ADV
+PrepNp
+NpaNp
+1812	notNPbutNP
+NpAdjp
+AdvpNp
+All-NP
+DetCL
+V-O
+DetNP
+PrepNp
+Np-Appos
+DetNP
+DetCL
+V-ADV
+PrepNp
+1826	P-VC-ADV
+PrepNp
+PrepNp
+DetNP
+1831	VC-P-S-ADV
+PrepNp
+NpPp
+NpNpNp
+Conj2Pp
+PrepNp
+Np-Appos
+1839	PrepNp
+Np-Appos
+Np-Appos
+DetNP
+NPofNP
+DetNP
+PrepNp
+NpaNp
+1850	sub-CL
+V-O
+1853	PrepNp
+DetNP
+NPofNP
+PrepNp
+sub-CL
+O-V-ADV
+PrepNp
+DetNP
+1862	Conj-CL
+1864	Np2CL
+V-IO-ADV
+notCLbutCL2CL
+sub-CL
+O2CL
+NpAdjp
+O-V-ADV
+PrepNp
+sub-CL
+V-O
+1875	Conj-CL
+1877	DetNP
+that-VP
+V-ADV
+PrepNp
+DetNP
+NPofNP
+1885	DetNP
+sub-CL
+V-ADV
+PrepNp
+that-VP
+ADV-V
+PrepNp
+1893	sub-CL
+V-ADV-S
+PrepNp
+DetNP
+Np-Appos
+AdjpNp
+DetCL
+ADV-V-O
+S-V-ADV
+Np-Appos
+PrepNp
+1905	S-VC-P
+NpaNp
+DetAdj
+1909	DetNP
+1912	sub-CL
+notCLbutCL2CL
+V-O
+O-V
+O-V
+NpAdjp
+1919	S-O-ADV-V
+All-NP
+DetCL
+CLaCL
+V2CL
+1925	ADV-V-ADV
+PrepNp
+DetNP
+NPofNP
+DetNP
+1932	Np2CL
+DetCL
+V-ADV
+PrepNp
+DetNP
+aNpaNp
+1939	DetNP
+1941	DetNP
+1944	sub-CL
+CLaCL
+S-V-ADV
+PrepNp
+1949	O-ADV-V
+Demo-NP
+DetNP
+ADV-V-O-ADV
+PrepNp
+1955	O-IO-ADV-V
+V2CL
+1958	Conj-CL
+S-V-ADV
+DetCL
+V-IO-O
+V2CL
+DetNP
+NPDetAdj
+NPofNP
+DetAdj
+1968	CLaCL
+ADV-ADV-V-ADV
+V-O
+O-IO-V
+PrepNp
+NpaNp
+1977	CLaCL
+VC-P
+PrepNp
+1981	ADV-V
+NpPp
+PrepNp
+sub-CL
+S-V
+DetNP
+NPofNP
+VerbBe
+1990	V-O-S
+DetNP
+NPofNP
+DetNP
+NPDetAdj
+NPofNP
+DetAdj
+1998	S-IO
+DetAdj
+NP-CL
+NPDetAdj
+DetAdj
+O-S-V-ADV
+PrepNp
+2007	Np2CL
+PrepNp
+S-V
+Conj2VP
+2012	sub-CL
+V-S
+ofNPNP
+DetNP
+2017	Conj-CL
+2020	V-S
+2022	V-ADV
+ofNPNP
+DetNP
+sub-CL
+S-ADV-V
+PrepNp
+2030	AdjpNp
+that-VP
+V-O
+S-ADV-V
+DetNP
+AdjpNp
+PrepNp
+DetNP
+2040	Np2CL
+O-V-ADV
+PtclCL
+O-V-ADV
+PrepNp
+DetNP
+2047	O-ADV
+NP-CL
+NP-CL
+S-V-ADV-ADV
+ofNPNP
+DetNP
+PrepNp
+ADV-ADV-V-ADV
+V-ADV-ADV
+DetNP
+2058	Conj-CL
+ADV-V-ADV
+PrepNp
+DetNP
+O-V-ADV
+PrepNp
+DetAdj
+2066	Conj-CL
+2068	V-O
+DetNP
+sub-CL
+P-VC-ADV
+DetNP
+2074	V-O-IO
+DetNP
+2077	Conj-CL
+S-ADV-V-O
+Np-Appos
+DetCL
+V-ADV
+2083	ADV-ADV-V-O
+PrepNp
+sub-CL
+V2CL
+NP-CL
+ofNPNP
+DetNP
+O-V-ADV
+ADV-V-O
+NpAdjp
+2094	Conj-CL
+2096	ADV-V-ADV
+PrepNp
+2099	S-V-O
+DetNP
+2102	O-V
+DetCL
+V2CL
+2106	ADV-V
+PrepNp
+DetNP
+2111	Np2CL
+V-O
+DetAdj
+O2CL
+DetAdj
+2117	S-P-VC
+DetCL
+V2CL
+PrepNp
+DetNP
+2123	S-ADV-V-O
+DetCL
+V2CL
+DetNP
+2128	ADV-V-ADV
+Conj2Pp
+PrepNp
+2132	PrepNp
+PronNP
+DetNP
+2136	Conj-CL
+CLaCL
+S-V
+AdvpNp
+2142	that-VP
+S-P-VC
+DetNP
+NPofNP
+2147	CLaCL
+V-O
+O-V-IO
+2151	ADV-V-O
+ADV-IO-V
+PrepNp
+NpaNp
+2157	Conj-CL
+CLaCL
+V-O
+ADV-O-V
+2162	ADV-V
+NpPp
+PrepNp
+2166	S-IO
+2168	V-O-S
+DetAdj
+2171	V-O-ADV
+DetAdj
+PrepNp
