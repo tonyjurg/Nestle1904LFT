1293cb050b170bff07d3c352d6fee2c12a00b6cb
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-04-24 13:23:46 +0200

    update version 0.1.3

---
+++ tf/0.1.3/otype.tf
@@ -0,0 +1,22 @@
+@node
+@Availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@Converter_author=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_execution=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_version=0.1.3 (recursive clause generation)
+@Convertor_source=https://github.com/tonyjurg/n1904_lft
+@Data source=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@Editors=Nestle
+@Name=Greek New Testament (NA1904)
+@TextFabric version=11.2.3
+@Version=1904
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2023-04-24T11:18:48Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067-169575	clause
+169576-240681	phrase
+240682-246405	sentence
+246406-254348	verse
