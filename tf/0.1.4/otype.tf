64042670ae4a4a72d3787c3b7a6bb4a0877abe97
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-05-01 11:34:40 +0200

    Add version 0.1.4
    Version which visualizes all wordgroups found in the low fat tree

---
+++ tf/0.1.4/otype.tf
@@ -0,0 +1,22 @@
+@node
+@Availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@Converter_author=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_execution=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_version=0.1.4 (visualize all wordgroups - recursive clause generation)
+@Convertor_source=https://github.com/tonyjurg/n1904_lft
+@Data source=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@Editors=Nestle
+@Name=Greek New Testament (NA1904)
+@TextFabric version=11.2.3
+@Version=1904
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2023-05-01T09:27:53Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067-261146	clause
+261147-332252	phrase
+332253-337976	sentence
+337977-345919	verse
