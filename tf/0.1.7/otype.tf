67ca7ff267d70809e981365c056531b74c81f1fc
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-05-03 20:38:26 +0200

    new version 0.1.7
    add role to each word

---
+++ tf/0.1.7/otype.tf
@@ -0,0 +1,21 @@
+@node
+@Availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@Converter_author=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_execution=Tony Jurg, Vrije Universiteit Amsterdam, Netherlands
+@Converter_version=0.1.7 (added role info to each word)
+@Convertor_source=https://github.com/tonyjurg/n1904_lft
+@Data source=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@Editors=Nestle
+@Name=Greek New Testament (NA1904)
+@TextFabric version=11.4.5
+@Version=1904
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2023-05-03T18:35:18Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067-146077	sentence
+146078-154020	verse
+154021-267471	wg
