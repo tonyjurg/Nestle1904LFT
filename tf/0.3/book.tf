d43ae5e1b2250d015acb8c437a67019acfc21407
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2023-06-19 17:28:48 +0200

    add tf 0.3 datafiles

---
+++ tf/0.3/book.tf
@@ -0,0 +1,42 @@
+@node
+@Availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@Converter_author=Tony Jurg, ReMa Student Vrije Universiteit Amsterdam, Netherlands
+@Converter_execution=Tony Jurg, ReMa Student Vrije Universiteit Amsterdam, Netherlands
+@Converter_version=0.3
+@Convertor_source=https://github.com/tonyjurg/Nestle1904LFT/tree/main/tools
+@Data source=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@Editors=Eberhard Nestle
+@Name=Greek New Testament (Nestle 1904 based on Low Fat Tree)
+@TextFabric version=11.4.10
+@description=Book
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2023-06-19T15:21:43Z
+
+137780	Matthew
+Mark
+Luke
+John
+Acts
+Romans
+I_Corinthians
+II_Corinthians
+Galatians
+Ephesians
+Philippians
+Colossians
+I_Thessalonians
+II_Thessalonians
+I_Timothy
+II_Timothy
+Titus
+Philemon
+Hebrews
+James
+I_Peter
+II_Peter
+I_John
+II_John
+III_John
+Jude
+Revelation
---
+++ tf/0.3/otype.tf
@@ -0,0 +1,20 @@
+@node
+@Availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@Converter_author=Tony Jurg, ReMa Student Vrije Universiteit Amsterdam, Netherlands
+@Converter_execution=Tony Jurg, ReMa Student Vrije Universiteit Amsterdam, Netherlands
+@Converter_version=0.3
+@Convertor_source=https://github.com/tonyjurg/Nestle1904LFT/tree/main/tools
+@Data source=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/lowfat
+@Editors=Eberhard Nestle
+@Name=Greek New Testament (Nestle 1904 based on Low Fat Tree)
+@TextFabric version=11.4.10
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2023-06-19T15:21:46Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067-146077	sentence
+146078-154020	verse
+154021-268899	wg
