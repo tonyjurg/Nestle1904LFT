c69f8183fc16a3b3c62721ce0f268562a3b32031
tonyjurg <github@jurgnet.nl>
2024-02-16 20:35:37 +0100

    add files for TF 0.7

---
+++ tf/0.7/otype.tf
@@ -0,0 +1,24 @@
+@node
+@author=Evangelists and apostles
+@availability=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@converterSource=https://github.com/tonyjurg/Nestle1904LFT/tree/main/resources/converter
+@converterVersion=0.7
+@converters=Tony Jurg
+@dataSource=MACULA Greek Linguistic Datasets, available at https://github.com/Clear-Bible/macula-greek/tree/main/Nestle1904/nodes
+@editors=Eberhart Nestle (1904)
+@sourceDescription=Greek New Testment (British Foreign Bible Society, 1904)
+@sourceFormat=XML (Low Fat tree XML data)
+@textFabriVersion=12.2.2
+@title=Greek New Testament (Nestle1904LFT)
+@valueType=str
+@xmlSourceDate=February 10, 2024
+@xmlSourceLocation=https://github.com/tonyjurg/Nestle1904LFT/tree/main/resources/xml/20240210
+@writtenBy=Text-Fabric
+@dateWritten=2024-02-16T19:29:59Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067-146077	sentence
+146078-154020	verse
+154021-259450	wg
